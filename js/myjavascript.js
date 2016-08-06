a1223fb52b772f5bbf46d12a0d0e5ec25a610055
zhai13 <4254001@qq.com>
2016-08-06 17:20:05 +0800

    获取元素，获取元素属性，设置元素属性，获取和设置文本节点，初步实现图片库功能

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -1,1 +1,8 @@
-alert('js 加载测试');
+function showPic(whichpic) {
+    var source = whichpic.getAttribute('href'); //获取元素属性
+    var text = whichpic.getAttribute('title');
+    var placeholder = document.getElementById('placeholder');   //获取元素
+    var description = document.getElementById('description');
+    placeholder.setAttribute('src', source);    //设置元素属性
+    description.firstChild.nodeValue = text;    //设置文本节点属性的值
+}
