3fcc4fc906ad328e634fa5b7c9c8fd4769255dfb
zhai13 <4254001@qq.com>
2016-08-06 19:35:44 +0800

    三元表达式，判断元素节点属性类型

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -4,11 +4,16 @@ function showPic(whichpic) {
     }
     var source = whichpic.getAttribute('href'); //获取元素属性
     var placeholder = document.getElementById('placeholder');   //获取元素
+    if (placeholder.nodeName != "IMG") {    //判断节点是否为图片类型
+        return false;
+    }
     placeholder.setAttribute('src', source);    //设置元素属性
     if (document.getElementById('description')) {
-        var text = whichpic.getAttribute('title');
+        var text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : "";    //三元表达式
         var description = document.getElementById('description');
-        description.firstChild.nodeValue = text;    //设置文本节点属性的值
+        if (description.firstChild.nodeType == 3) { //判断节点属性是否为文本节点
+            description.firstChild.nodeValue = text;    //设置文本节点属性的值
+        }
     }
     return true;
 }
