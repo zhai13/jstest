d184dfbe6142bb4cc032eaae8923468017171acd
php4 <4254001@qq.com>
2016-08-09 21:46:22 +0800

    index test ajax js文件和html文件分开

--- js/index.js
+++ js/index.js
@@ -76,19 +76,3 @@ function showPic(whichpic) {
 //页面加载完毕后执行下面两个函数
 addLoadEvent(prepareGallery);
 addLoadEvent(preparePlaceholder);
-
-/*test.html*/
-/*
-window.onload = function() {
-    var para = document.createElement('p');
-    var text1 = document.createTextNode('This is ');
-    para.appendChild(text1);
-    var emphasis =  document.createElement('em');
-    var text2 = document.createTextNode('my');
-    emphasis.appendChild(text2);
-    para.appendChild(emphasis);
-    var text3 = document.createTextNode(' content.');
-    para.appendChild(text3);
-    var testdiv = document.getElementById('testdiv');
-    testdiv.appendChild(para);
-}*/
