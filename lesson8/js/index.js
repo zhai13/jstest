78eb114103d99cbb09a236a698a298654a610fe9
php4 <4254001@qq.com>
2016-08-12 14:28:13 +0800

    将abbr的title和文本组合动态生成一个新的代码块，并展示在文档中

--- lesson8/js/index.js
+++ lesson8/js/index.js
@@ -8,6 +8,23 @@ function displayAbbreviations() {
         var key = current_abbr.lastChild.nodeValue;
         defs[key] = definition;
     }
-    console.log(defs);
+    var dlist = document.createElement('dl');
+    //for...in 语句用于遍历数组或者对象的属性（对数组或者对象的属性进行循环操作）。
+    for (key in defs) {
+        var defintion = defs[key];  //取addr的title
+        var dtitle = document.createElement('dt');  //创建<dt></dt>
+        var dtitle_text = document.createTextNode(key); //创建文本
+        dtitle.appendChild(dtitle_text);    //将文本插入<dt></dt>中间
+        var ddesc = document.createElement('dd');   //创建dd
+        var ddesc_text =  document.createTextNode(defintion);   //创建文本（addr的title）
+        ddesc.appendChild(ddesc_text);  //将文本插入<dd></dd>中间
+        dlist.appendChild(dtitle);  //将<dt></dt>插入dl中
+        dlist.appendChild(ddesc);  //将<dd></dd>插入dl中
+    }
+    var header = document.createElement('h2');  //创建h2元素
+    var header_text = document.createTextNode('Abbreviations'); //创建文本
+    header.appendChild(header_text);    //将文本插入至<h2></h2>中
+    document.body.appendChild(header);  //将h2插入文档中
+    document.body.appendChild(dlist);  //将dl插入文档中
 }
-displayAbbreviations();
+displayAbbreviations(); //函数调用
