28734ad4085cc11cfac5e5bfbcf9a4a4d6683d87
php4 <4254001@qq.com>
2016-08-18 14:05:29 +0800

    通过style属性去设置（添加/覆盖）属性值

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,5 +1,5 @@
 var para = document.getElementById('example');
-console.log(para.style.color);
-console.log(para.style.fontFamily);
-//总结，通过style属性获得样式有很大的局限性，他只能获得内嵌样式
-//margin-top,在js读取style属性应为，marginTop（技巧，将'-'去掉，'-'后面第一个字母大写，驼峰式写法）
+console.log(para.style.color);  //获取样式
+console.log(para.style.fontFamily); //font-family驼峰写法
+para.style.color = 'black'; //设置颜色样式（如果有样式则为覆盖，否则为添加）
+para.style.fontSize = '30px';   //添加一个属性样式
