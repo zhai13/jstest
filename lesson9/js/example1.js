be4adf4fe86ed9653d4dfe0f8e5c16d8a06605d8
php4 <4254001@qq.com>
2016-08-24 13:50:07 +0800

    根据元素在节点数里的位置设置样式(添加注释)

--- lesson9/js/example1.js
+++ lesson9/js/example1.js
@@ -3,24 +3,24 @@ function styleHeaderSiblings() {
     var elem;
     for (var i=0; i<headers.length; i++) {
         elem = getNextElement(headers[i].nextSibling);  //文档中的下一个节点可以用nextSibling查找出来
-        addClass(elem, 'intro');
+        addClass(elem, 'intro');    //给元素附加一个class名
     }
 }
 function addClass(emelemt,value) {
-    if (!emelemt.className) {
+    if (!emelemt.className) {   //如果当前元素没有class名就将‘intro’作为class名附加给当前元素
         emelemt.className = value;
     } else {
-        emelemt.className +=" ";
+        emelemt.className +=" ";    //如果当前元素有class名就在此class名后追加‘intro’class名
         emelemt.className +=value;
     }
 }
 function getNextElement(node) {
-    if (node.nodeType == 1) {
+    if (node.nodeType == 1) {   //如果值元素节点，直接返回元素节点
         return node;
     }
     if (node.nextSibling) {
-        return getNextElement(node.nextSibling);
+        return getNextElement(node.nextSibling);    //如果存在子节点就继续查找下一个子节点
     }
     return null;
 }
-window.onload = styleHeaderSiblings;
+window.onload = styleHeaderSiblings;    //页面加载完毕后执行
