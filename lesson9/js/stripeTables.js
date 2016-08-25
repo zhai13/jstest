a8c817846dcc9b182bcf8df049ebc39dbf46efff
php4 <4254001@qq.com>
2016-08-25 22:32:44 +0800

    动态隔行换色

--- lesson9/js/stripeTables.js
+++ lesson9/js/stripeTables.js
@@ -1,1 +1,17 @@
-//TODO
+function stripeTables() {
+    var tables = document.getElementsByTagName('table');
+    var odd, rows;
+    for (var i=0; i<tables.length; i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName('tr');
+        for (var j=0; j<rows.length; j++) {
+            if (odd = true) {
+                rows[j].style.backgroundColor = '#ffc';
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+window.onload = stripeTables;    //页面加载完毕后执行
