4626fcc92b61308e470eb3672c9ed9a5e72b1c12
php4 <4254001@qq.com>
2016-08-26 14:30:23 +0800

    动态增加class名改变样式实现隔行换色的效果

--- lesson9/js/stripeTables.js
+++ lesson9/js/stripeTables.js
@@ -1,17 +1,27 @@
 function stripeTables() {
-    var tables = document.getElementsByTagName('table');
-    var odd, rows;
+    if (!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
     for (var i=0; i<tables.length; i++) {
-        odd = false;
-        rows = tables[i].getElementsByTagName('tr');
+        var odd = false;
+        var rows = tables[i].getElementsByTagName("tr");
         for (var j=0; j<rows.length; j++) {
-            if (odd = true) {
-                rows[j].style.backgroundColor = '#ffc';
+            if (odd == true) {
+                addClass(rows[j],"odd");
                 odd = false;
             } else {
                 odd = true;
             }
         }
+    }
+}
+function addClass(element,value) {
+    if (!element.className) {
+        element.className = value;
+    } else {
+        newClassName = element.className;
+        newClassName+= " ";
+        newClassName+= value;
+        element.className = newClassName;
     }
 }
 window.onload = stripeTables;    //页面加载完毕后执行
